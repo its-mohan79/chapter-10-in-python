49e7212e0ad2db98ba9015929c63a11476ba46cd
its-mohan79 <mohanroy8989@gmail.com>
2026-01-14 23:50:53 +0530

    complete the lesson 1

--- 01_class.py
+++ 01_class.py
@@ -1,1 +1,17 @@
-MOHAN
+class employee:
+    language = "py"  #this is a class attribute
+    salary =120000
+
+
+mohan = employee()
+mohan.name = "mohan"  #this is an instance attribute
+print(mohan.name, mohan.language, mohan.salary)
+
+
+rohan = employee()
+rohan.name = "rohan roro robison"
+print(rohan.name, rohan.salary, rohan.language)
+
+
+#here name is instance attribute and salary and language
+#attributes as they directly belong to the class
