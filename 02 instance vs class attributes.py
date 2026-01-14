de4db35de1fc658f0381eac146bfb505ca04fb1c
its-mohan79 <mohanroy8989@gmail.com>
2026-01-14 23:51:27 +0530

    complete the lesson 2

---
+++ 02 instance vs class attributes.py
@@ -0,0 +1,10 @@
+class employee:
+    language = "py"  #this is a class attribute
+    salary =120000
+
+
+mohan = employee()
+mohan.language= "javascript"  #this is an instance attribute
+print( mohan.language, mohan.salary)
+
+
